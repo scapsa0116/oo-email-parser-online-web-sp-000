c24ed8f8fda5ef955b7038eed7b5822fbc2a8b6a
Stela Capsa <stela.capsa@gmail.com>
2020-01-04 15:45:32 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,7 @@ class EmailAddressParser
   end 
   
   def parse 
-    csv_emails.split.collect do |address|
+    .split.collect do |address|
       address.split(',') 
     end
     .flatten.uniq 
